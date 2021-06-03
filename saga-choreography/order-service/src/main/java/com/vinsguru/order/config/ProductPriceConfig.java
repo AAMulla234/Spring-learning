aa0b6e60c62eaec7baf8e184a2ed2bb1b21710cd
Akhtarjahan Qureshi <qureshia@pmc.local>
2021-06-03 12:16:35 +0530

    Saga choreography

---
+++ saga-choreography/order-service/src/main/java/com/vinsguru/order/config/ProductPriceConfig.java
@@ -0,0 +1,22 @@
+package com.vinsguru.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ProductPriceConfig {
+
+    // product price map
+    @Bean
+    public Map<Integer, Integer> productPrice() {
+        return new HashMap<Integer, Integer>(){{
+            put(1,100);
+            put(2,200);
+            put(3,300);
+        }};
+    }
+
+}
